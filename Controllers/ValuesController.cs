c6dd7301797d638bcda1a4eecaaa2de46d0a80fe
Alistair Chapman <alistair@agchapman.com>
2017-12-01 18:40:30 +1000

    Initial commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ddd_api.Controllers
+{
+    [Route("api/ddd")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet("{location}")]
+        public IActionResult Get(string location)
+        {
+            switch (location.ToLower())
+            {
+                case "bne":
+                case "brisbane":
+                    return Ok(new{
+                        Location = "UQ, St Lucia",
+                        Date = DateTime.Today.ToShortDateString(),
+                        IsBestCity = true
+                    });
+                case "syd":
+                case "sydney":
+                    return Ok(new {
+                        Location = "UTS, Sydney",
+                        Date = new DateTime(2017,7,15).ToShortDateString(),
+                        IsBestCity = false
+                    });
+                default:
+                    return NotFound();
+            }
+        }
+    }
+}
